d5cb8047bd4aab14dba3f575b8efb0b6b1a7faf1
lucopdev <lucopdev@gmail.com>
2022-11-23 23:36:15 -0300

    Adicionando pastas da seção 4 dia 01

---
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-01-primeiros-passos/exercicio01/script.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = 8;
+let operator = '+';
+
+if(operator === '+'){
+    console.log(a + b);
+}
+if(operator === '-'){
+    console.log(a - b);
+}
+if(operator === '/'){
+    console.log(a / b);
+}
+if(operator === '*'){
+    console.log(a * b);
+}
