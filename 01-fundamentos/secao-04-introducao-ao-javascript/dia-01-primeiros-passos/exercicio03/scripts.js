047e662097caf76d20ea00730b510a0f6704bc29
lucopdev <lucopdev@gmail.com>
2022-11-24 09:12:38 -0300

    Exercicio03

---
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-01-primeiros-passos/exercicio03/scripts.js
@@ -0,0 +1,13 @@
+const valor1 = 10;
+const valor2 = 7;
+const valor3 = 25;
+
+if (valor1 > valor2 && valor1 > valor3) {
+    console.log(`${valor1} é maior que ${valor2} e ${valor3}`);
+}
+if (valor2 > valor1 && valor2 > valor3) {
+    console.log(`${valor2} é maior que ${valor1} e ${valor3}`);
+}
+if (valor3 > valor1 && valor3 > valor2) {
+    console.log(`${valor3} é maior que ${valor1} e ${valor2}`);
+}
