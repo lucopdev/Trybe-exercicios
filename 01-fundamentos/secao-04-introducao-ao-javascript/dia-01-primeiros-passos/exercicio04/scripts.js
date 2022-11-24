9ab89163bad41e17b2d25d010b58c84e7e2dca28
lucopdev <lucopdev@gmail.com>
2022-11-24 09:25:04 -0300

    Exercicio04

---
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-01-primeiros-passos/exercicio04/scripts.js
@@ -0,0 +1,11 @@
+const valor = -245;
+
+if (valor > 0) {
+    console.log(`${valor} is positive`);
+}
+if (valor < 0) {
+    console.log(`${valor} is negative`);
+}
+if (valor === 0) {
+    console.log(`${valor} is ZERO`);
+}
