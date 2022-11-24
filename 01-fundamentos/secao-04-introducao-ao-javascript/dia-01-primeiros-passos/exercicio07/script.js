889647f168021bb4d91c0ad3e5c06a9fd00d5d63
lucopdev <lucopdev@gmail.com>
2022-11-24 11:04:28 -0300

    Exercicio 7 e 8

---
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-01-primeiros-passos/exercicio07/script.js
@@ -0,0 +1,9 @@
+const num = 15;
+const num2 = 25;
+const num3 = 51;
+
+if (num % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+    console.log('true');
+}else{
+    console.log('false');
+}
---
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-01-primeiros-passos/exercicio08/script.js
@@ -0,0 +1,9 @@
+const num = 10;
+const num2 = 20;
+const num3 = 51;
+
+if (num % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0) {
+    console.log('true');
+}else{
+    console.log('false');
+}
