4889577d6149162280d05deee49e8add5ae1da43
lucopdev <lucopdev@gmail.com>
2022-11-25 15:11:18 -0300

    exercícios para fixar dia 02 - for of

--- 01-fundamentos/secao-04-introducao-ao-javascript/dia-02-array-for/parafixar01.js
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-02-array-for/parafixar01.js
@@ -7,3 +7,8 @@ menu.push('Contato');
 console.log(menu);
 console.log(menuServices);
 console.log(indexOsPortifolio);
+
+const names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for(nome of names){
+    console.log(nome);
+}
