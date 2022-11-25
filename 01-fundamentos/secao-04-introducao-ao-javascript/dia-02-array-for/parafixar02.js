a58fc9f0b44d86bd9fcadbc7dc3f587cc6643305
lucopdev <lucopdev@gmail.com>
2022-11-25 18:23:28 -0300

    Exercicios de array e for of 2

---
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-02-array-for/parafixar02.js
@@ -0,0 +1,24 @@
+// let counter = 0;
+
+// while(counter !== 5) {
+//   console.log('Hello Wolrd!');
+//     counter += 1;
+// }
+
+
+// function dice () { 
+//     return Math.ceil(Math.random() * 6);
+// }
+// let d1 = dice();
+// let d2 = dice();
+// while (d1 === d2) {
+//     console.log(d1, d2);
+//     d2 = dice();
+// }
+// console.log(d1, d2);
+
+
+let numeros = [1, 2, 3, 4, 5];
+
+numeros.pop();
+console.log(numeros);
