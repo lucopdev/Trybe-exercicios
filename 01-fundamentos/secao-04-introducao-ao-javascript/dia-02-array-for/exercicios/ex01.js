8a14f2e1e5865a30f35db0f2007db3777fc48cab
lucopdev <lucopdev@gmail.com>
2022-11-25 20:15:27 -0300

    Exercicios de array e for of - 6

--- 01-fundamentos/secao-04-introducao-ao-javascript/dia-02-array-for/exercicios/ex01.js
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-02-array-for/exercicios/ex01.js
@@ -46,6 +46,12 @@ let counter = 0;
 for (value of numbers) {
     if (value % 2 !== 0) {
         counter++;
+        if (counter === 0) {
+            console.log('Nenhum valor encontrado');
+        }
     }
 }
 console.log(counter);
+
+/////////////////////////////////////////////////////////
+
