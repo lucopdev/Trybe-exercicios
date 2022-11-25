5e7212598169ea24814156a56f71f5e174b70703
lucopdev <lucopdev@gmail.com>
2022-11-25 19:45:09 -0300

    Exercicios de array e for of - 5

--- 01-fundamentos/secao-04-introducao-ao-javascript/dia-02-array-for/exercicios/ex01.js
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-02-array-for/exercicios/ex01.js
@@ -27,3 +27,17 @@ if (average > 20) {
 } else {
     console.log('valor menor ou igual a 20');
 }
+
+/////////////////////////////////////////////////////////
+
+//[5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+
+let maxValue = 0
+for (let value = 0; value < numbers.length; value += 1) {
+    if (numbers[value] > maxValue){
+        maxValue = numbers[value];
+    }
+}
+console.log(maxValue);
+
+/////////////////////////////////////////////////////////
