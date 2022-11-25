af05d8b99e4cd60c94d5acc6eb4e228fbdac0197
lucopdev <lucopdev@gmail.com>
2022-11-25 15:07:19 -0300

    exercícios para fixar dia 02

---
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-02-array-for/parafixar01.js
@@ -0,0 +1,9 @@
+let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
+let menuServices = menu[1];
+let indexOsPortifolio = menu.indexOf('Portifólio');
+
+menu.push('Contato');
+
+console.log(menu);
+console.log(menuServices);
+console.log(indexOsPortifolio);
