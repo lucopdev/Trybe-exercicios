8cbfd4a49c7c8bf725bcd5238061c8fb193f4d66
lucopdev <lucopdev@gmail.com>
2022-11-26 08:36:52 -0300

    Exercicios de array e for of - 7

--- 01-fundamentos/secao-04-introducao-ao-javascript/dia-02-array-for/exercicios/exercicios.js
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-02-array-for/exercicios/exercicios.js
@@ -1,9 +1,11 @@
+// 01
+//[5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 console.log(numbers);
 
 /////////////////////////////////////////////////////////
-
+//02
 let sum = 0;
 for (values of numbers) {
     sum += values;
@@ -12,7 +14,7 @@ for (values of numbers) {
 console.log(sum);
 
 /////////////////////////////////////////////////////////
-
+//03
 let average = 0;
 for (values of numbers) {
     sum += values;
@@ -21,7 +23,7 @@ for (values of numbers) {
 console.log(average);
 
 /////////////////////////////////////////////////////////
-
+//04
 if (average > 20) {
     console.log('valor maior que 20!');
 } else {
@@ -29,10 +31,9 @@ if (average > 20) {
 }
 
 /////////////////////////////////////////////////////////
-
+//05
 //[5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
-
-let maxValue = 0
+let maxValue = 0;
 for (let value = 0; value < numbers.length; value += 1) {
     if (numbers[value] > maxValue) {
         maxValue = numbers[value];
@@ -41,7 +42,7 @@ for (let value = 0; value < numbers.length; value += 1) {
 console.log(maxValue);
 
 /////////////////////////////////////////////////////////
-
+//06
 let counter = 0;
 for (value of numbers) {
     if (value % 2 !== 0) {
@@ -54,4 +55,25 @@ for (value of numbers) {
 console.log(counter);
 
 /////////////////////////////////////////////////////////
+//07
+//[5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+let minValue = numbers[0];
+for (let index = 0; index < numbers.length; index += 1) {
+    if (minValue > numbers[index]) {
+        minValue = numbers[index];
+    }
+}
+console.log(minValue);
 
+/////////////////////////////////////////////////////////
+//08
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////
+//[5, 9, 3, 19, 70, 8, 100, 2, 35, 27, 9, 3, 1]
+// contar quantos valores repetidos tem no array
