10a7fb7ec27180550f13b9860abbc773487ade8e
lucopdev <lucopdev@gmail.com>
2022-11-26 19:04:25 -0300

    Editando readme principal

--- 01-fundamentos/secao-04-introducao-ao-javascript/dia-02-array-for/ordenacao-buble-sort/obs.js
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-02-array-for/ordenacao-buble-sort/obs.js
@@ -1,17 +1,17 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// Exercício 01:
-// for (let secondIndex = 1; secondIndex < numbers.length; secondIndex++) {
+//Exercício 01:
+for (let secondIndex = 1; secondIndex < numbers.length; secondIndex++) {
 
-//     for (let index = 0; index < secondIndex; index++) {
-//         if (numbers[secondIndex] < numbers[index]) {
-//             let position = numbers[index];
-//             numbers[index] = numbers[secondIndex];
-//             numbers[secondIndex] = position;
-//         }
-//     }
-// }
-// console.log(numbers);
+    for (let index = 0; index < secondIndex; index++) {
+        if (numbers[secondIndex] < numbers[index]) {
+            let position = numbers[index];
+            numbers[index] = numbers[secondIndex];
+            numbers[secondIndex] = position;
+        }
+    }
+}
+console.log(numbers);
 
 ///////////////////////////////////////////////////////////////////////////////////
 
@@ -31,21 +31,17 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 ////////////////////////////////////////////////////////////////////////////////////
 
 // Exercício 03:
-let numbersTwice = [];
-let numberX2 = 0;
-for (let secondIndex = 1; secondIndex <= numbers.length; secondIndex++) {
-    for (let index = 0; index < secondIndex; index++) {
-        if (secondIndex === numbers.length) {
-            numberX2 = numbers[index] * 2;
-        } else {
-            numberX2 = numbers[index] * numbers[secondIndex];
-        }
-    }
-    
-    numbersTwice.push(numberX2);
-}
-console.log(numbersTwice);
+// let numbersTwice = [];
+// let numberX2 = 0;
+// for (let secondIndex = 1; secondIndex <= numbers.length; secondIndex++) {
+//     for (let index = 0; index < secondIndex; index++) {
+//         if (secondIndex === numbers.length) {
+//             numberX2 = numbers[index] * 2;
+//         } else {
+//             numberX2 = numbers[index] * numbers[secondIndex];
+//         }
+//     }
 
-
-
-
+//     numbersTwice.push(numberX2);
+// }
+// console.log(numbersTwice);
