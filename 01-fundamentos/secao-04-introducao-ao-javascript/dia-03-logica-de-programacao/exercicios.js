b1662e2b59cafd3b63a92e29889f76682cdea292
lucopdev <lucopdev@gmail.com>
2022-11-29 19:21:02 -0300

    lógica de programação exercicio 2

--- 01-fundamentos/secao-04-introducao-ao-javascript/dia-03-logica-de-programacao/exercicios.js
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-03-logica-de-programacao/exercicios.js
@@ -1,6 +1,16 @@
-let number = 10;
-let factorial = number;
-for (let index = 1; index < number; index += 1) {
-    factorial = factorial * (number-index);    
+// // 01
+// let number = 10;
+// let factorial = number;
+// for (let index = 1; index < number; index += 1) {
+//     factorial = factorial * (number-index);    
+// }
+// console.log(factorial);
+
+// 02
+let word = 'TRYBER';
+let newWord = '';
+let allLetters = word.length
+for (let index = 1; index <= allLetters; index++) {
+    newWord = newWord + word[allLetters-index];
 }
-console.log(factorial);
+console.log(newWord);
