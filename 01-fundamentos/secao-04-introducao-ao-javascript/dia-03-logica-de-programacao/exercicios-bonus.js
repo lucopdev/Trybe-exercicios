471a9553a5c43d5ac9cba43d81efdc9c0e67f63d
lucopdev <lucopdev@gmail.com>
2022-11-30 11:29:59 -0300

    lógica de programação exercicio BÔNUS 1 e 2 CORREÇÃO

--- 01-fundamentos/secao-04-introducao-ao-javascript/dia-03-logica-de-programacao/exercicios-bonus.js
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-03-logica-de-programacao/exercicios-bonus.js
@@ -2,25 +2,37 @@
 
 // let n = 5;
 // let draw = '';
-// for (let index = 0; index < n; index++) {
+// for (let index = 0; index < n; index += 1) {
 //     draw = draw + '*';
 // }
-// for (let index = 0; index < n; index++) {
+// for (let index = 0; index < n; index += 1) {
 //     console.log(draw);
 // }
 
 
 // 02
-let n = 5;
-let draw = '';
-for (let index = 0; index < n; index++) {
-    draw = draw + '*';
-    console.log(draw);
-}
+// let n = 5;
+// let draw = '';
+// for (let index = 0; index < n; index += 1) {
+//     draw = draw + '*';
+//     console.log(draw);
+// }
 
 // 03
-// let n = 5;
-// for (let index = 0; index < array.length; index++) {
-
+let n = 5;
+let draw = '*';
+let space = ' ';
+for (let index = n; index > 0; index -= 1) {
     
+    
+    console.log(space, draw);
+}
+
+// 04
+// let n = 5;
+// let draw = '*';
+// let space = ' ';
+// for (let index = 0; index < n ; index += 1) {
+//     space = space + ' ';    
+//     console.log(space, draw);
 // }
