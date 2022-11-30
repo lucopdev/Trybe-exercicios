904755432c7cef249fa399f81f639a9139d1de35
lucopdev <lucopdev@gmail.com>
2022-11-30 15:43:48 -0300

    objeto e funções - algoritimo do Carro

---
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-04-objetos-e-funcoes/exercicios.js
@@ -0,0 +1,21 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function minus(a, b) {
+    return a - b;
+}
+
+function division(a, b) {
+    return a / b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+function module(a, b) {
+    return a % b;
+}
+
+console.log(sum(10, 50));
