817195b57dee46ef4be59d06e320a063826a9437
lucopdev <lucopdev@gmail.com>
2022-11-30 15:52:17 -0300

    objeto e funções - maior numero

--- 01-fundamentos/secao-04-introducao-ao-javascript/dia-04-objetos-e-funcoes/exercicios.js
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-04-objetos-e-funcoes/exercicios.js
@@ -1,21 +1,32 @@
-function sum(a, b) {
-    return a + b;
-}
+// 01
+// function sum(a, b) {
+//     return a + b;
+// }
 
-function minus(a, b) {
-    return a - b;
-}
+// function minus(a, b) {
+//     return a - b;
+// }
 
-function division(a, b) {
-    return a / b;
-}
+// function division(a, b) {
+//     return a / b;
+// }
 
-function multiplication(a, b) {
-    return a * b;
-}
+// function multiplication(a, b) {
+//     return a * b;
+// }
 
-function module(a, b) {
-    return a % b;
-}
+// function module(a, b) {
+//     return a % b;
+// }
 
-console.log(sum(10, 50));
+// console.log(sum(10, 50));
+
+// 02
+function higherNumber(num1, num2) {
+    if (num1 > num2) {
+        return `O maior é o ${num1}`;
+    } else {
+        return `O maior é o ${num2}`;
+    }
+}
+console.log(higherNumber(30, 40));
