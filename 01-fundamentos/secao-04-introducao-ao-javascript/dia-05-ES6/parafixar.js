701b3dab9f8b377f096cac00088d992ac0829a2a
lucopdev <lucopdev@gmail.com>
2022-12-01 12:57:55 -0300

    Fixando conteúdo de ES6

---
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-05-ES6/parafixar.js
@@ -0,0 +1,14 @@
+// const wordCounter = (frase) => console.log(frase.split(' ').length);
+// wordCounter('Eu amo pizza de peperoni');
+
+// const objetoPessoa = (nome, idade) => ({
+//     nome: nome,
+//     idade: idade,
+// });
+
+// const verifica = (2 + 1 === 2) ? 'verdade' : 'mentira';
+// console.log(verifica);
+
+// const checkAge = (age) => age > 18 ? 'True' : 'False';
+// console.log(checkAge(25));
+
