278590228889296c3877bf1407a9e563593f76c4
lucopdev <lucopdev@gmail.com>
2022-11-30 21:02:34 -0300

    Exercicios de funções palindromo finalizado

--- 01-fundamentos/secao-04-introducao-ao-javascript/dia-04-objetos-e-funcoes/funcoes.js
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-04-objetos-e-funcoes/funcoes.js
@@ -1,19 +1,16 @@
 function palindrome(word) {
-    let size = word.length-1;
+    let size = word.length - 1;
     let palindrome = 0;
     for (let index = 0; index < word.length; index++) {
         for (let index = 0; index < word.length; index++) {
-            if (word[index] === word[size-index]) {
-                palindrome += 1;
-            }else{
+            if (word[index] !== word[size - index]) {
                 return 'False';
             }
-            if(palindrome === word.lengh/2)
-            return 'True';
         }
     }
+    return 'True';
 }
-console.log(palindrome('arara'));
+console.log(palindrome('ubararabu'));
 
 // let word = 'arara';
 // let size = word.length-1;
