ed1d9ee91607ca182ba30ea1b025862672278b18
lucopdev <lucopdev@gmail.com>
2022-12-01 09:42:34 -0300

    Exercicios de funções - Maior nome do array

--- 01-fundamentos/secao-04-introducao-ao-javascript/dia-04-objetos-e-funcoes/funcoes.js
+++ 01-fundamentos/secao-04-introducao-ao-javascript/dia-04-objetos-e-funcoes/funcoes.js
@@ -49,3 +49,17 @@
 // }
 // console.log(higherNumber([2, 4, 6, 7, 10, 0, -3]));
 
+//04
+//['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']
+// function names(array) {
+//     let higherName = 0;
+//     for (let index = 0; index < array.length; index++) {
+//         for (let innerIndex = 0; innerIndex < array.length; innerIndex++) {
+//             if(array[innerIndex].length > array[index].length){
+//                 higherName = array[innerIndex];
+//             }
+//         }
+//     }
+//     return higherName;
+// }
+// console.log(names(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
